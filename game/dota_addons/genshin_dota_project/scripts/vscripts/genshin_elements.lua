da09982b1fe8f7bc2ec9fe76305368020d3ac41c
Silvian-73 <yurykuz65@gmail.com>
2021-09-28 21:25:12 +0300

    Started making vaporize reaction

--- game/dota_addons/genshin_dota_project/scripts/vscripts/genshin_elements.lua
+++ game/dota_addons/genshin_dota_project/scripts/vscripts/genshin_elements.lua
@@ -16,6 +16,8 @@ GenshinElements.ELECTRO = 4
 GenshinElements.ANEMO = 5
 GenshinElements.GEO = 6
 GenshinElements.DENDRO = 7
+GenshinElements.MIN_ELEMENT = 1
+GenshinElements.MAX_ELEMENT = 7
 
 GenshinElements.DEFAULT_ELEMENT_DURATIONS =
 {
@@ -25,6 +27,16 @@ GenshinElements.DEFAULT_ELEMENT_DURATIONS =
     [GenshinElements.ELECTRO] = 5,
     [GenshinElements.ANEMO] = 5,
     [GenshinElements.GEO] = 5
+}
+
+GenshinElements.ELEMENTAL_MODIFIER_NAMES =
+{
+    [GenshinElements.PYRO] = "modifier_pyro_effect",
+    [GenshinElements.HYDRO] = "modifier_hydro_effect",
+    [GenshinElements.CRYO] = "modifier_cryo_effect",
+    [GenshinElements.ELECTRO] = "modifier_electro_effect",
+    [GenshinElements.ANEMO] = "modifier_anemo_effect",
+    [GenshinElements.GEO] = "modifier_geo_effect"
 }
 
 function GenshinElements:ApplyElementalDamage(damageTable)
@@ -48,16 +60,39 @@ function GenshinElements:ApplyElement(args)
     if args.caster == nil then error("caster is nil") end
     if args.target == nil then error("target is nil") end
     if args.element == nil then error("element is nil") end
+
     args.duration = args.duration or self.DEFAULT_ELEMENT_DURATION
-    args.target:AddNewModifier( args.caster, nil, self:ElementToModifierName(args.element), { duration = self.DEFAULT_ELEMENT_DURATIONS[args.element] } )
-    return 1
+    args.target:AddNewModifier( args.caster, nil, self.ELEMENTAL_MODIFIER_NAMES[args.element], { duration = self.DEFAULT_ELEMENT_DURATIONS[args.element] } )
+
+    local damageMuliplier = 1
+    if(self:UnitHasElementalModifiers(target, self.PYRO, self.HYDRO)) then
+        print("Vaporize triggered by element " .. args.element)
+        self:RemoveElementalModifiersFromUnit(target, self.PYRO, self.HYDRO)
+    end
+
+    return damageMuliplier
 end
 
-function GenshinElements:ElementToModifierName(element)
-    if element == self.PYRO then return "modifier_pyro_effect"
-    elseif element == self.HYDRO then return "modifier_hydro_effect"
-    elseif element == self.CRYO then return "modifier_cryo_effect"
-    elseif element == self.ELECTRO then return "modifier_electro_effect"
-    elseif element == self.ANEMO then return "modifier_anemo_effect"
-    elseif element == self.GEO then return "modifier_geo_effect" end
+function GenshinElements:UnitHasElementalModifier(unit, element)
+    return unit:FindModifierByName(self.ELEMENTAL_MODIFIER_NAMES[element]) ~= nil
+end
+
+function GenshinElements:UnitHasElementalModifiers(unit, ...)
+    for k, v in pairs{...} do
+        if not self:UnitHasElementalModifier(unit, v) then
+            return false
+        end
+    end
+
+    return true
+end
+
+function GenshinElements:RemoveElementalModifierFromUnit(unit, element)
+    unit:RemoveModifierByName(self.ELEMENTAL_MODIFIER_NAMES[element])
+end
+
+function GenshinElements:RemoveElementalModifiersFromUnit(unit, ...)
+    for k, v in pairs{...} do
+        self:RemoveElementalModifierFromUnit(v)
+    end
 end
